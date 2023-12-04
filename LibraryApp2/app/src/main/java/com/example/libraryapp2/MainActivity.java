980e19e99f0926e5f14fe407e3ae61d69c86629c
Sak <jacubsak@gmail.com>
2023-12-04 23:37:49 +0100

    zad7 mobilki

---
+++ LibraryApp2/app/src/main/java/com/example/libraryapp2/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.libraryapp2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.libraryapp2.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private BookViewModel bookViewModel;
+
+    private Book editedBook = null;
+    public static final int NEW_BOOK_ACTIVITY_REQUEST_CODE = 1;
+    public static final int EDIT_BOOK_ACTIVITY_REQUEST_CODE = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final BookAdapter adapter = new BookAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        bookViewModel = new ViewModelProvider(this).get(BookViewModel.class);
+        bookViewModel.findAll().observe(this, adapter::setBooks);
+
+
+
+        FloatingActionButton addBookButton = findViewById(R.id.add_button);
+        addBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditBookActivity.class);
+                startActivityForResult(intent, NEW_BOOK_ACTIVITY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class BookHolder extends RecyclerView.ViewHolder{
+        private TextView bookTitle;
+        private TextView bookAuthor;
+        public BookHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.book_list_item, parent, false));
+            bookTitle = itemView.findViewById(R.id.book_title);
+            bookAuthor = itemView.findViewById(R.id.book_author);
+
+        }
+        public void bind(Book book){
+            bookTitle.setText(book.getTitle());
+            bookAuthor.setText(book.getAuthor());
+        }
+
+    }
+    private class BookAdapter extends RecyclerView.Adapter<BookHolder>{
+        private List<Book> books;
+        @NonNull
+        @Override
+        public BookHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+            return new BookHolder(getLayoutInflater(), parent);
+        }
+        @Override
+        public void onBindViewHolder(@NonNull BookHolder holder, int position){
+            if(books !=null){
+                Book book = books.get(position);
+                holder.bind(book);
+            }else{
+                Log.d("MainActivity", "No Books");
+            }
+        }
+        @Override
+        public int getItemCount(){
+            if(books!=null){
+                return books.size();
+            }else{
+                return 0;
+            }
+        }
+        void setBooks(List<Book> books){
+            this.books = books;
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == NEW_BOOK_ACTIVITY_REQUEST_CODE) {
+                Book book = new Book(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE),
+                        data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR));
+                bookViewModel.insert(book);
+                Snackbar.make(findViewById(R.id.coordinator_layout),
+                                getString(R.string.book_added),
+                                Snackbar.LENGTH_LONG)
+                                .show();
+            }
+            else if (requestCode == EDIT_BOOK_ACTIVITY_REQUEST_CODE) {
+                editedBook.setTitle(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE));
+                editedBook.setAuthor(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR));
+                bookViewModel.update(editedBook);
+                editedBook = null;
+                Snackbar.make(findViewById(R.id.coordinator_layout),
+                                getString(R.string.book_edited),
+                                Snackbar.LENGTH_LONG)
+                                .show();
+            }
+        }
+        else
+            Snackbar.make(findViewById(R.id.coordinator_layout),
+                            getString(R.string.empty_not_saved),
+                            Snackbar.LENGTH_LONG)
+                            .show();
+    }
+}
